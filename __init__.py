f2206e8d6d98c422000f5462a22bf3f032ad60bc
snockow6 <nst8021@hotmail.com>
2026-07-25 09:39:20 +1200

    initial commit: VNCCS Control Center custom node with QIE2511 text encoder support

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""VNCCS Control Center Edited - lightweight clone of the original VNCCS Control Center node."""
+
+from .nodes.vnccs_control_center_edited import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+
+WEB_DIRECTORY = "web"
---
+++ nodes/__init__.py
@@ -0,0 +1,3 @@
+from .vnccs_control_center_edited import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
